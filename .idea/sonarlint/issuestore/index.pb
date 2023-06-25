718e831659f2c2f2fde226128feac333f1683900
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-06-25 20:42:00 +0330

    Dashboard right side buttons modified.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+R
+"web/app/admin/dashboard/layout.tsx,4/f/4fe14e3b01b0a76b81c1b3c0fc3a1d365e9e8013
--- web/components/admin/dashboard/RightSide.tsx
+++ web/components/admin/dashboard/RightSide.tsx
@@ -6,52 +6,74 @@ import LocalTaxi from "@mui/icons-material/LocalTaxi";
 import FamilyRestroom from "@mui/icons-material/FamilyRestroom";
 import Link from "next/link";
 import { AccountBalance, Newspaper } from "@mui/icons-material";
+import { useRouter, usePathname } from "next/navigation";
 
 const RightSide = () => {
+  const routerPath = usePathname();
+
+  // Function to check if a specific route is active
+  const isRouteActive = (pathname: string) => {
+    return routerPath === pathname;
+  };
+
   return (
     <div className="basis-1/4 bg-white w-full h-screen">
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <Dashboard sx={{ my: 0.3 }} />
-          <Link href={`/admin/dashboard/`}>شروع</Link>
+      <Link href={`/admin/dashboard/`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <Dashboard sx={{ my: 0.3 }} />
+            شروع
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <School sx={{ my: 0.3 }} />
-          <Link href={`/admin/dashboard/provinces`}>استان ها</Link>
+      </Link>
+      <Link href={`/admin/dashboard/provinces`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/provinces") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <School sx={{ my: 0.3 }} />
+            استان ها
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <School sx={{ my: 0.3 }} />
-          <Link href={`/admin/dashboard/schools`}>مدرسه ها</Link>
+      </Link>
+      <Link href={`/admin/dashboard/schools`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/schools") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <School sx={{ my: 0.3 }} />
+            مدرسه ها
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <LocalTaxi sx={{ my: 0.3 }} />
-          <Link href={`/admin/dashboard/drivers`}>مدیریت رانندگان</Link>
+      </Link>
+      <Link href={`/admin/dashboard/drivers`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/drivers") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <LocalTaxi sx={{ my: 0.3 }} />
+            مدیریت رانندگان
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink rightMenuLinkActive">
-        <div className="rightMenuLinkInternally">
-          <FamilyRestroom sx={{ my: 0.3 }} />
-          <Link href={`/admin/dashboard/parents`}>اولیا</Link>
+      </Link>
+      <Link href={`/admin/dashboard/parents`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/parents") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <FamilyRestroom sx={{ my: 0.3 }} />
+            اولیا
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <AccountBalance sx={{ my: 0.3 }} />
-          <Link href={`/`}>حسابداری</Link>
+      </Link>
+      <Link href={`/`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <AccountBalance sx={{ my: 0.3 }} />
+            حسابداری
+          </div>
         </div>
-      </div>
-      <div className="rightMenuLink ">
-        <div className="rightMenuLinkInternally">
-          <Newspaper sx={{ my: 0.3 }} />
-          <Link href={`/`}>اخبار</Link>
+      </Link>
+      <Link href={`/`}>
+        <div className={`rightMenuLink ${isRouteActive("/admin/dashboard/") ? "rightMenuLinkActive" : ""}`}>
+          <div className="rightMenuLinkInternally">
+            <Newspaper sx={{ my: 0.3 }} />
+            اخبار
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
